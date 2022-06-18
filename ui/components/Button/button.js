335a123361ba72a77d9173a0dab95926e527bbe9
Mohammad Marandi <mohammadmarandi89@gmail.com>
2022-06-18 22:39:25 +0430

    fix design

--- ui/components/Button/button.js
+++ ui/components/Button/button.js
@@ -1,11 +1,22 @@
 export default function Button(props) {
-  const { type, value, name, text } = props;
+  const { type, value, name, text, width } = props;
   return (
     <button
       type={type}
       value={value}
       name={name}
-      className="bg-[#5053ff] hover:bg-[#5003fd] active:bg-[#f45 py-3 rounded-lg cursor-pointer hover:bg-violet-500 duration-200 text-slate-100 font-bold text-sm"
+      style={{ width: width }}
+      className="
+      bg-[#5053ff] 
+      hover:bg-[#5003fd] 
+      py-3 
+      rounded-lg 
+      cursor-pointer 
+      duration-200 
+      text-slate-100 
+      font-bold 
+      text-sm
+      "
     >
       {text}
     </button>
