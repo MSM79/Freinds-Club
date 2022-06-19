7e123753730363b182a8812b29f4a6bf56265cd7
msm1379 <mohammadmarandi89@gmail.com>
2022-06-19 10:06:49 +0430

    fixed header link

--- ui/components/Header/index.jsx
+++ ui/components/Header/index.jsx
@@ -9,43 +9,54 @@ export default function Navbar({ fixed }) {
       <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-white mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <a
-              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-[#5053ff]"
-              href="#pablo"
-            >
-              Freinds Club
-            </a>
+            <Link href="/">
+              <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-[#5053ff]">
+                Freinds Club
+              </a>
+            </Link>
+
             <button
-              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              className="text-dark cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
               onClick={() => setNavbarOpen(!navbarOpen)}
             >
-              <i className="fas fa-bars"></i>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
             </button>
           </div>
           <div
             className={
-              'lg:flex flex-grow items-center' + (navbarOpen ? ' flex' : ' hidden')
+              'lg:flex flex-grow items-center' +
+              (navbarOpen ? ' flex' : ' hidden')
             }
             id="example-navbar-danger"
           >
             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
               <li className="nav-item">
-                <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-gray-700"
-                  href="#pablo"
-                >
-                  <Button text="Sign in" width="100px" />
-                </a>
+                <Link href="/login">
+                  <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-gray-700">
+                    <Button text="Sign in" width="100px" />
+                  </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-gray-700"
-                  href="#pablo"
-                >
-                  <button
-                    type="button"
-                    className="
+                <Link href="/signup">
+                  <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-gray-700">
+                    <button
+                      type="button"
+                      className="
                       bg-white
                       hover:text-slate-900
                       py-3 
@@ -57,10 +68,11 @@ export default function Navbar({ fixed }) {
                       text-sm
                       w-24
                       "
-                  >
-                    Sign Up
-                  </button>
-                </a>
+                    >
+                      Sign Up
+                    </button>
+                  </a>
+                </Link>
               </li>
             </ul>
           </div>
