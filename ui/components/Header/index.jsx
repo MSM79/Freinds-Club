8d66216267578471656e661cd0d2fde207d7c1fe
Mohammad Marandi <mohammadmarandi89@gmail.com>
2022-06-27 21:45:22 +0430

    add search to header

--- ui/components/Header/index.jsx
+++ ui/components/Header/index.jsx
@@ -10,7 +10,7 @@ export default function Navbar({ fixed }) {
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <Link href="/">
-              <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-[#5053ff]">
+              <a className="text-md font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-[#5053ff]">
                 Freinds Club
               </a>
             </Link>
@@ -36,10 +36,49 @@ export default function Navbar({ fixed }) {
               </svg>
             </button>
           </div>
+
+          <form className="w-3/4 flex justify-center xs:w-full">
+            <div className="relative">
+              <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                <svg
+                  className="w-6 h-6 text-gray-400"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  ></path>
+                </svg>
+              </div>
+              <input
+                type="search"
+                id="default-search"
+                className="block 
+                p-4 
+                pl-12
+                h-12 
+                w-full 
+                text-xl 
+                text-gray-900 
+                bg-gray-100 
+                rounded-2xl
+                shadow-outline
+                outline-none 
+                "
+                placeholder="Search"
+                required
+              />
+            </div>
+          </form>
+
           <div
             className={
-              'lg:flex flex-grow items-center' +
-              (navbarOpen ? ' flex' : ' hidden')
+              'lg:flex flex-grow items-center' + (navbarOpen ? ' flex' : ' hidden')
             }
             id="example-navbar-danger"
           >
