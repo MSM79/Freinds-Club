f6960b933dbd548d9f46cfe7133a4776c9de8271
Mohammad Marandi <mohammadmarandi89@gmail.com>
2022-07-13 14:28:53 +0430

    fix user route

--- pages/user/[id]/index.js
+++ pages/user/[id]/index.js
@@ -1,16 +1,63 @@
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import Image from 'next/image';
 
-const User = () => {
+function User(props) {
+  const { name, username, title } = props;
   const router = useRouter();
   const { id } = router.query;
 
   return (
     <>
       <h1>user: {id}</h1>
-      <ul></ul>
+      <div className="relative max-w-md mx-auto md:max-w-2xl mt-6 min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-xl mt-16">
+        <div className="px-6">
+          <div className="flex flex-wrap justify-center">
+            <div className="w-full flex justify-center">
+              <div className="relative">
+                <img
+                  src="https://github.com/creativetimofficial/soft-ui-dashboard-tailwind/blob/main/build/assets/img/team-2.jpg?raw=true"
+                  className="shadow-xl rounded-full align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-[150px]"
+                />
+              </div>
+            </div>
+            <div className="w-full text-center mt-20">
+              <div className="flex justify-center lg:pt-4 pt-8 pb-0">
+                <div className="p-3 text-center">
+                  <span className="text-xl font-bold block uppercase tracking-wide text-slate-700">
+                    3,360
+                  </span>
+                  <span className="text-sm text-slate-400">Photos</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="text-center mt-2">
+            <h3 className="text-2xl text-slate-700 font-bold leading-normal mb-1">
+              {name}
+            </h3>
+            <div className="text-xs mt-0 mb-2 text-slate-400 font-bold uppercase">
+              <i className="fas fa-map-marker-alt mr-2 text-slate-400 opacity-75"></i>
+              {username}
+            </div>
+          </div>
+          <div className="mt-6 py-6 border-t border-slate-200 text-center">
+            <div className="flex flex-wrap justify-center">
+              <div className="w-full px-4">
+                <p className="font-light leading-relaxed text-slate-600 mb-4">{title}</p>
+                <a
+                  href="javascript:;"
+                  className="font-normal text-slate-700 hover:text-slate-400"
+                >
+                  Follow Account
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
-};
+}
 
 export default User;
